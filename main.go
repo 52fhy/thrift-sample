6744b7aee27b341ef104b332819af1acce57c72f
yujc <yujc@youshu.cc>
2019-07-06 23:57:50 +0800

    add go test

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ type Greeter struct {
 
 func (this *Greeter) SayHello(ctx context.Context, u *Sample.User) (r *Sample.Response, err error) {
 	strJson, _ := json.Marshal(u)
-	return &Sample.Response{ErrCode: 0, ErrMsg: "success", Data: map[string]string{"data": string(strJson)}}, nil
+	return &Sample.Response{ErrCode: 0, ErrMsg: "success", Data: map[string]string{"User": string(strJson)}}, nil
 }
 
 func (this *Greeter) GetUser(ctx context.Context, uid int32) (r *Sample.Response, err error) {
